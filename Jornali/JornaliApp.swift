c889b3da6bda6729f3af9c030509af81074e27bf
Asayi1 <assail116134@gmail.com>
2025-10-16 15:13:34 +0300

    Initial Commit

---
+++ Jornali/JornaliApp.swift
@@ -0,0 +1,17 @@
+//
+//  JornaliApp.swift
+//  Jornali
+//
+//  Created by Asail abdulmohsin on 24/04/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct JornaliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
